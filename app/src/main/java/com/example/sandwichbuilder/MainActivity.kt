3c2dd647fb367f065bcb23327ad0a5a534cb26e2
ericdecanini <eddecanini@gmail.com>
2022-07-20 10:08:07 +0200

    adds kotlin android extensions

--- app/src/main/java/com/example/sandwichbuilder/MainActivity.kt
+++ app/src/main/java/com/example/sandwichbuilder/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.sandwichbuilder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
