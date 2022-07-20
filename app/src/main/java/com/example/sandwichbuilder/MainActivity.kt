ce82cd8b6dfbf1201ef92737bd0fbc4b45779a73
ericdecanini <eddecanini@gmail.com>
2022-07-20 10:20:15 +0200

    Adds integer numberOfSandwiches

--- app/src/main/java/com/example/sandwichbuilder/MainActivity.kt
+++ app/src/main/java/com/example/sandwichbuilder/MainActivity.kt
@@ -6,12 +6,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var numberOfSandwiches = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         add_button.setOnClickListener {
-            println("Add button works!")
+            numberOfSandwiches = numberOfSandwiches + 1
+            println("Number of sandwiches: " + numberOfSandwiches)
         }
     }
 }
