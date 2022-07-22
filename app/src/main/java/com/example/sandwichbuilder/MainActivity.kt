5bd625a908f93c88aad0315aac6cbe9d4120ec11
ericdecanini <eddecanini@gmail.com>
2022-07-22 15:55:00 +0200

    Removes string usage

--- app/src/main/java/com/example/sandwichbuilder/MainActivity.kt
+++ app/src/main/java/com/example/sandwichbuilder/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         add_button.setOnClickListener {
             numberOfSandwiches = numberOfSandwiches + 1
-            println("Number of sandwiches: " + numberOfSandwiches)
+            println(numberOfSandwiches)
         }
     }
 }
